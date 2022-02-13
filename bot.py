7c61d1793464f599533783e8a3704b95b1e7de3f
delphython <delphython@gmail.com>
2022-02-13 22:48:24 +0300

    Change backslash

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 def main():
-    with open("quiz-questions\1vs1200.txt", "r", encoding="koi8-r") as file:
+    with open("quiz-questions/1vs1200.txt", "r", encoding="koi8-r") as file:
         file_contents = file.read()
 
     print(file_contents)
