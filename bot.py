c9583af2e6a96ca3f815b0f58c11598df833878f
delphython <delphython@gmail.com>
2022-02-13 22:45:21 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,9 @@
+def main():
+    with open("quiz-questions\1vs1200.txt", "r", encoding="koi8-r") as file:
+        file_contents = file.read()
+
+    print(file_contents)
+
+
+if __name__ == "__main__":
+    main()
