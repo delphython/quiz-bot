f2f41c61f649bd99d18c7056f6b598b151ff2c28
delphython <delphython@gmail.com>
2022-02-14 12:24:00 +0300

    Split text in file

--- bot.py
+++ bot.py
@@ -2,7 +2,11 @@ def main():
     with open("quiz-questions/1vs1200.txt", "r", encoding="koi8-r") as file:
         file_contents = file.read()
 
-    print(file_contents)
+    for file_section in file_contents.split("\n\n\n"):
+        print("-------------------------------------------")
+        for question_section in file_section.split("\n\n"):
+            print(question_section)
+            print("______________________________________")
 
 
 if __name__ == "__main__":
