82d3f2efe63590fa7a2b62715917d1d99f8030e9
delphython <delphython@gmail.com>
2022-02-15 20:33:09 +0300

    Add answer checking

--- bot.py
+++ bot.py
@@ -42,57 +42,66 @@ def get_questions_and_answers(filename):
     return questions_and_answers
 
 
-def start(bot, update):
+def start(update, context):
     custom_keyboard = [
         ["Новый вопрос", "Сдаться"],
         ["Мой счет"],
     ]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-    bot.send_message(
+    context.bot.send_message(
         chat_id=update.message.chat_id,
         text="Привет! Я бот для викторин!",
         reply_markup=reply_markup,
     )
 
 
-def help(bot, update):
+def help(update, context):
     update.message.reply_text("Help!")
 
 
-def echo(bot, update):
-    # update.message.reply_text(update.message.text)
+def echo(update, context):
+    questions_and_answers = context.bot_data
     randome_question = random.choice(list(questions_and_answers.keys()))
+
     if update.message.text == "Новый вопрос":
         update.message.reply_text(randome_question)
+        context.user_data["current_question"] = randome_question
+    else:
+        answer = questions_and_answers[context.user_data["current_question"]]
+        smart_answer = answer.split("(")[0].split(".")[0]
+        if update.message.text == smart_answer:
+            update.message.reply_text(
+                "Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»"
+            )
+        else:
+            update.message.reply_text("Неправильно… Попробуешь ещё раз?")
 
 
-def error(bot, update, error):
+def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def main():
     load_dotenv()
 
-    global questions_and_answers
-
     telegram_token = os.getenv("TELEGRAM_TOKEN")
     quiz_questions_file = os.getenv("QUIZ_QUESTIONS_FILE")
-    redis_host = os.getenv("REDIS_HOST")
-    redis_port = os.getenv("REDIS_PORT")
-    redis_pass = os.getenv("REDIS_PASS")
+    # redis_host = os.getenv("REDIS_HOST")
+    # redis_port = os.getenv("REDIS_PORT")
+    # redis_pass = os.getenv("REDIS_PASS")
 
-    redis_connection = redis.Redis(
-        host=redis_host, port=redis_port, password=redis_pass, db=0
-    )
+    # redis_connection = redis.Redis(
+    #     host=redis_host, port=redis_port, password=redis_pass, db=0
+    # )
 
     questions_and_answers = get_questions_and_answers(quiz_questions_file)
-    randome_question = random.choice(list(questions_and_answers.keys()))
+    # randome_question = random.choice(list(questions_and_answers.keys()))
 
-    redis_connection.set("1", randome_question)
-    redis_connection.set("2", randome_question)
-
-    print(redis_connection.get("1"))
-    print(redis_connection.get("2"))
+    # redis_connection.set("1", randome_question)
+    # redis_connection.set("2", randome_question)
+    #
+    # print(redis_connection.get("1"))
+    # print(redis_connection.get("2"))
 
     updater = Updater(telegram_token)
 
