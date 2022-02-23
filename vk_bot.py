749685f41df67a82988647c187820ccf62850129
delphython <delphython@gmail.com>
2022-02-23 13:52:58 +0300

    Add quiz question text in message

--- vk_bot.py
+++ vk_bot.py
@@ -1,5 +1,6 @@
 import os
 import random
+import re
 
 import vk_api
 
@@ -9,36 +10,67 @@ from vk_api.utils import get_random_id
 from dotenv import load_dotenv
 
 
-def echo(event, vk_api):
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1, 1000)
-    )
+def get_questions_and_answers(filename):
+    questions_and_answers = {}
+    question_pattern = re.compile("Вопрос \d*:\n")
+    answer_pattern = re.compile("Ответ:\n")
+
+    with open(filename, "r", encoding="koi8-r") as file:
+        file_contents = file.read()
+
+    for file_section in file_contents.split("\n\n\n"):
+        question, answer = None, None
+        for question_section in file_section.split("\n\n"):
+            if question_pattern.match(question_section):
+                question = question_pattern.split(question_section)[1]
+            if answer_pattern.match(question_section):
+                answer = answer_pattern.split(question_section)[1]
+        if (question is not None) and (answer is not None):
+            questions_and_answers[question] = answer
+
+    return questions_and_answers
 
 
 if __name__ == "__main__":
     load_dotenv()
 
     vk_token = os.getenv("VK_TOKEN")
+    quiz_questions_file = os.getenv("QUIZ_QUESTIONS_FILE")
+
+    questions_and_answers = get_questions_and_answers(quiz_questions_file)
 
     vk_session = vk_api.VkApi(token=vk_token)
     vk = vk_session.get_api()
 
     keyboard = VkKeyboard(one_time=True)
 
-    keyboard.add_button('Синяя кнопка', color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button('Зелёная кнопка', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_button("Новый вопрос", color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button("Сдаться", color=VkKeyboardColor.POSITIVE)
 
-    keyboard.add_line()  # Переход на вторую строку
-    keyboard.add_button('Красная кнопка', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button("Мой счет", color=VkKeyboardColor.NEGATIVE)
 
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            vk.messages.send(
-                user_id=event.user_id,
-                random_id=get_random_id(),
-                keyboard=keyboard.get_keyboard(),
-                message='Пример клавиатуры'
-            )
+            if event.text == "/start":
+                vk.messages.send(
+                    user_id=event.user_id,
+                    random_id=get_random_id(),
+                    keyboard=keyboard.get_keyboard(),
+                    message="Привет! Я бот для викторин!"
+                )
+            elif event.text == "Новый вопрос":
+                randome_question = random.choice(
+                    list(questions_and_answers.keys())
+                )
+                vk.messages.send(
+                    user_id=event.user_id,
+                    random_id=get_random_id(),
+                    keyboard=keyboard.get_keyboard(),
+                    message=randome_question
+                )
+            elif event.text == "Сдаться":
+                pass
+            elif event.text == "Мой счет":
+                pass
